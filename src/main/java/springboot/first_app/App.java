3565ba7822d2aeb2dc6f87086b684d643ed196c6
Harish S <hsatya@gmail.com>
2021-03-19 10:14:43 -0400

    Update App.java

--- src/main/java/springboot/first_app/App.java
+++ src/main/java/springboot/first_app/App.java
@@ -1,5 +1,6 @@
 package springboot.first_app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,11 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableAutoConfiguration
 public class App
-{
+
+
+{     
+	
+	@Value("${message}")
+private String message;
 	
 	 @RequestMapping("/")
 	    String home() {
-	        return "Hello World Spring Boot!";
+	        return message;
 	    }
 public static void main( String[] args )
 {
